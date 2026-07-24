1863f2fd309f983b86e9937261f590e011a48270
Jan Sommerfeld <jan.sommerfeld@snowflake.com>
2026-07-24 09:59:38 +0200

    Remove unnecessary grants from access.sql
    Removed grants for streamlits and semantic views in access.sql.

--- Quickstarts/DCM_Project_Quickstart_1/sources/definitions/access.sql
+++ Quickstarts/DCM_Project_Quickstart_1/sources/definitions/access.sql
@@ -22,9 +22,6 @@ grant USAGE on schema DCM_DEMO_1{{env_suffix}}.SERVE     to role DCM_DEMO_1{{env
 
 -- Inherited grants and container-level MANAGE GRANTS require the following account-level opt-in (Public Preview):
 -- ALTER ACCOUNT SET FEATURE_RBAC_INHERITED_GRANTS = 'ENABLED';
-
 grant INHERITED SELECT on all tables         in schema DCM_DEMO_1{{env_suffix}}.SERVE to role DCM_DEMO_1{{env_suffix}}_READ;
 grant INHERITED SELECT on all dynamic tables in schema DCM_DEMO_1{{env_suffix}}.SERVE to role DCM_DEMO_1{{env_suffix}}_READ;
 grant INHERITED SELECT on all views          in schema DCM_DEMO_1{{env_suffix}}.SERVE to role DCM_DEMO_1{{env_suffix}}_READ;
-grant INHERITED USAGE on all streamlits      in schema DCM_DEMO_1{{env_suffix}}.SERVE to role DCM_DEMO_1{{env_suffix}}_READ;
-grant INHERITED SELECT on all semantic views  in schema DCM_DEMO_1{{env_suffix}}.SERVE to role DCM_DEMO_1{{env_suffix}}_READ;
